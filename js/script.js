34f8828e0275d81c4e2dd4fced92ab9100735df4
Túlio V <34729643+tuliovp@users.noreply.github.com>
2021-04-14 20:34:23 -0300

    education

--- js/script.js
+++ js/script.js
@@ -15,3 +15,33 @@ function toggleClass(el){
     el.className = classes.join(' ');
   }
 }
+
+function toggletext() {
+  const x = document.getElementById("email");
+  if (x.innerHTML === "Email") {
+    x.innerHTML = "tulio.vasconcelos1@gmail.com";
+  } else {
+    x.innerHTML = "Email";
+  }
+}
+
+function scrollToSmoothly(pos, time) {
+  var currentPos = window.pageYOffset;
+  var start = null;
+  if(time == null) time = 500;
+  pos = +pos, time = +time;
+  window.requestAnimationFrame(function step(currentTime) {
+      start = !start ? currentTime : start;
+      var progress = currentTime - start;
+      if (currentPos < pos) {
+          window.scrollTo(0, ((pos - currentPos) * progress / time) + currentPos);
+      } else {
+          window.scrollTo(0, currentPos - ((currentPos - pos) * progress / time));
+      }
+      if (progress < time) {
+          window.requestAnimationFrame(step);
+      } else {
+          window.scrollTo(0, pos);
+      }
+  });
+}
