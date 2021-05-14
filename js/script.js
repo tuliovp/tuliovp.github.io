68efe4a162b74234a5a45364241bb42ba6b6119a
Túlio V <34729643+tuliovp@users.noreply.github.com>
2021-05-14 20:51:05 -0300

    cleaner modal

--- js/script.js
+++ js/script.js
@@ -65,6 +65,9 @@ if(this.className == 'is-playing'){
 
 });
 
+
+// CERTIFICATIONS MODAL
+
 //
 // Open the Modal
 function openModal() {
@@ -111,69 +114,4 @@ function showSlides(n) {
 }
 
 
-// PORTFOLIO
 
-// Get the modal
-var modal = document.getElementById("modal_portfolio");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close_portfolio")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
-
-// CERTIFICATIONS TEST
-
-// Get the button that opens the modal
-var btn = document.querySelectorAll("button.modal-button");
-
-// All page modals
-var modals = document.querySelectorAll('.modal');
-
-// Get the <span> element that closes the modal
-var spans = document.getElementsByClassName("close");
-
-// When the user clicks the button, open the modal
-for (var i = 0; i < btn.length; i++) {
- btn[i].onclick = function(e) {
-    e.preventDefault();
-    modal = document.querySelector(e.target.getAttribute("href"));
-    modal.style.display = "block";
- }
-}
-
-// When the user clicks on <span> (x), close the modal
-for (var i = 0; i < spans.length; i++) {
- spans[i].onclick = function() {
-    for (var index in modals) {
-      if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";    
-    }
- }
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target.classList.contains('modal')) {
-     for (var index in modals) {
-      if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";    
-     }
-    }
-}
