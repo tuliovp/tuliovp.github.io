4819b2f59b1f31b2cf5fc970bcc593a5b36e7a39
Túlio V <34729643+tuliovp@users.noreply.github.com>
2021-05-26 17:53:39 -0300

    final organisation part 2

--- js/script.js
+++ js/script.js
@@ -141,9 +141,9 @@ var btnContainer = document.getElementById("myBtnContainer");
 var btns = btnContainer.getElementsByClassName("btn1");
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
+    var current = document.getElementsByClassName("active1");
+    current[0].className = current[0].className.replace(" active1", "");
+    this.className += " active1";
   });
 }
 
